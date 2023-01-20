69b9be4cf7f7e0344df4b292afdf46476376f15d
Martin Sjöberg <essjaybee@gmail.com>
2023-01-20 23:14:13 +0100

    iterate job list instead of units, remove workers when possible

--- canceljobs.lua
+++ canceljobs.lua
@@ -4,22 +4,27 @@
 canceljobs
 ========
 
-Cancels all jobs/tasks.
+Removes workers from assigned jobs.
+When not possible it removes the job from the task list.
 
 Usage::
 
     canceljobs
 
 ]====]
-local job_count = 0;
+
+local utils = require 'utils'
+local job_count = 0
 
 if df.global.gamemode ~= df.game_mode.DWARF or not dfhack.isMapLoaded() then
     qerror('canceljobs requires a fortress map to be loaded')
 end
 
-for _, unit in pairs(df.global.world.units.active) do
-    if unit.job.current_job then
-        dfhack.job.removeJob(unit.job.current_job)
+for _, job in utils.listpairs(df.global.world.jobs.list) do
+    if dfhack.job.getWorker(job) then
+        if not dfhack.job.removeWorker(job, 1) then
+            dfhack.job.removeJob(job)
+        end
         job_count = job_count + 1
     end
 end
